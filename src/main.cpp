953a23fe947885bc87505efee2116fa43521de65
Felipe Miiller <felipemiillerr@gmail.com>
2020-07-26 21:20:32 -0400

    menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// Inicializa o display no endereco 0x27
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+ 
+void setup()
+{
+ lcd.begin (16,2);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+ 
+void loop()
+{
+  lcd.setCursor(0,0);
+  lcd.print("********************");
+  lcd.setCursor(2,1);
+  lcd.print("Hello World");
+  
 }
